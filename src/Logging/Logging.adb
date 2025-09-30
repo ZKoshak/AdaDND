31d3202b799399e520ef98ec34f4ef85f76e9ab9
ZKoshak <artyom.krukovsky@gmail.com>
2025-09-30 14:26:15 +0300

    Last Sync: 2025-09-30 14:26 (Mobile)

--- src/Logging/Logging.adb
+++ src/Logging/Logging.adb
@@ -1,58 +1,114 @@
 package body Logging is
-   
-   procedure Initialize_Logging(File_Name : String) is
-   begin
-      Ada.Text_IO.Create(Log_File, Ada.Text_IO.Out_File, File_Name);
-      if not Ada.Text_IO.Is_Open(Log_File) then
-   Put_Line("Ошибка при инициализации логирования!");
-   return;
-end if;
-   end Initialize_Logging;
-   
-   procedure Finalize_Logging is
-   begin
-      if Ada.Text_IO.Is_Open(Log_File) then
-         Ada.Text_IO.Close(Log_File);
-      end if;
-   end Finalize_Logging;
-   
-   procedure Log(Message : String; Level : Log_Level := INFO) is
-      Current_Time : constant String := Ada.Calendar.Format(Ada.Calendar.Clock);
-   begin
-      if Level >= Current_Level then
-         declare
-            Formatted_Message : constant String := 
-               Current_Time & " [" & Log_Level'Image(Level) & "] " & Message;
-         begin
-            Ada.Text_IO.Put_Line(Formatted_Message);
-            Ada.Text_IO.Put_Line(Log_File, Formatted_Message);
-         end;
-      end if;
-   end Log;
-   
-   procedure Debug(Message : String) is
-   begin
-      Log(Message, DEBUG);
-   end Debug;
-   
-   procedure Info(Message : String) is
-   begin
-      Log(Message, INFO);
-   end Info;
-   
-   procedure Warning(Message : String) is
-   begin
-      Log(Message, WARNING);
-   end Warning;
-   
-   procedure Error(Message : String) is
-   begin
-      Log(Message, ERROR);
-   end Error;
-   
-   procedure Set_Log_Level(Level : Log_Level) is
-   begin
-      Current_Level := Level;
-   end Set_Log_Level;
-   
+    
+    -- Инициализация логирования
+    procedure Initialize_Logging(Filename : String) is
+    begin
+        Create(Log_File, Out_File, Filename);
+        Set_Log_Level(DEBUG);
+        Log("--- Начало лога ---", INFO);
+    end Initialize_Logging;
+    
+    -- Установка уровня логирования
+    procedure Set_Log_Level(Level : Log_Level) is
+    begin
+        Current_Log_Level := Level;
+        Log("Установлен уровень логирования: " & Integer'Image(Level), INFO);
+    end Set_Log_Level;
+    
+    -- Завершение логирования
+    procedure Finalize_Logging is
+    begin
+        if Is_Open(Log_File) then
+            Log("--- Конец лога ---", INFO);
+            Close(Log_File);
+        end if;
+    end Finalize_Logging;
+    
+    -- Основная процедура записи в лог
+    procedure Log(Message : String; Level : Log_Level) is
+    begin
+        if Level >= Current_Log_Level then
+            declare
+                Formatted_Message : String := Format_Message(Level, Message);
+            begin
+                Put_Line(Log_File, Formatted_Message);
+                -- Опционально: вывод в консоль
+                Put_Line(Formatted_Message);
+            end;
+        end if;
+    end Log;
+    
+    -- Обёртки для разных уровней
+    procedure Debug(Message : String) is
+    begin
+        Log(Message, DEBUG);
+    end Debug;
+    
+    procedure Info(Message : String) is
+    begin
+        Log(Message, INFO);
+    end Info;
+    
+    procedure Warning(Message : String) is
+    begin
+        Log(Message, WARNING);
+    end Warning;
+    
+    procedure Error(Message : String) is
+    begin
+        Log(Message, ERROR);
+    end Error;
+    
+    procedure Critical(Message : String) is
+    begin
+        Log(Message, CRITICAL);
+    end Critical;
+    
+    procedure Fatal(Message : String) is
+    begin
+        Log(Message, FATAL);
+    end Fatal;
+    
+    -- Получение текущего времени
+    function Get_Current_Time return String is
+        use Ada.Calendar;
+        Current : Time := Clock;
+        Year, Month, Day : Integer;
+        Hour, Minute, Second : Integer;
+    begin
+        Split(Current, Year, Month, Day, Hour, Minute, Second);
+        return Year'Image & "-" & Month'Image & "-" & Day'Image & " " &
+              Hour'Image & ":" & Minute'Image & ":" & Second'Image;
+    end Get_Current_Time;
+    
+    -- Форматирование сообщения
+    function Format_Message(Level : Log_Level; Message : String) return String is
+        Level_Name : String;
+    begin
+        case Level is
+            when DEBUG => Level_Name := "DEBUG";
+            when INFO => Level_Name := "INFO";
+            when WARNING => Level_Name := "WARNING";
+            when ERROR => Level_Name := "ERROR";
+            when CRITICAL => Level_Name := "CRITICAL";
+            when FATAL => Level_Name := "FATAL";
+            when others => Level_Name := "UNKNOWN";
+        end case;
+
+        return Get_Current_Time & " " & Level_Name & " " & Message;
+        end Format_Message;
+
+    -- Обработка исключений
+    procedure Handle_Logging_Error(Msg : String) is
+    begin
+        -- Простая обработка ошибок
+        Put_Line("Ошибка логирования: " & Msg);
+    end Handle_Logging_Error;
+
+    -- Дополнительные функции
+    function Is_Logging_Enabled return Boolean is
+    begin
+        return Current_Log_Level >= 0;
+    end Is_Logging_Enabled;
+
 end Logging;
--- src/Logging/Logging.ads
+++ src/Logging/Logging.ads
@@ -1,16 +1,33 @@
 package Logging is
-   type Log_Level is (DEBUG, INFO, WARNING, ERROR);
-   Current_Level : Log_Level := INFO;
-   Log_File : Ada.Text_IO.File_Type;
-   
-   procedure Initialize_Logging(File_Name : String);
-   procedure Finalize_Logging;
-   
-   procedure Log(Message : String; Level : Log_Level := INFO);
-   procedure Debug(Message : String);
-   procedure Info(Message : String);
-   procedure Warning(Message : String);
-   procedure Error(Message : String);
-   
-   procedure Set_Log_Level(Level : Log_Level);
+    -- Уровни логирования
+    subtype Log_Level is Integer range 0..5;
+    DEBUG : constant Log_Level := 5;
+    INFO : constant Log_Level := 4;
+    WARNING : constant Log_Level := 3;
+    ERROR : constant Log_Level := 2;
+    CRITICAL : constant Log_Level := 1;
+    FATAL : constant Log_Level := 0;
+    
+    -- Типы и константы
+    type Log_File_Type is file of Character;
+    Log_File : Log_File_Type;
+    Current_Log_Level : Log_Level := DEBUG;
+    
+    -- Процедуры инициализации
+    procedure Initialize_Logging(Filename : String);
+    procedure Set_Log_Level(Level : Log_Level);
+    procedure Finalize_Logging;
+    
+    -- Основные процедуры логирования
+    procedure Log(Message : String; Level : Log_Level);
+    procedure Debug(Message : String);
+    procedure Info(Message : String);
+    procedure Warning(Message : String);
+    procedure Error(Message : String);
+    procedure Critical(Message : String);
+    procedure Fatal(Message : String);
+    
+    -- Вспомогательные функции
+    function Get_Current_Time return String;
+    function Format_Message(Level : Log_Level; Message : String) return String;
 end Logging;
