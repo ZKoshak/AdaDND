b18c2084a01d63ff05200af301f59897415cb870
ZKoshak <artyom.krukovsky@gmail.com>
2025-09-30 12:21:49 +0300

    Last Sync: 2025-09-30 12:21 (Mobile)

--- Main.adb
+++ Main.adb
@@ -3,58 +3,73 @@ with Ada.Text_IO; use Ada.Text_IO;
 with Game_Core; use Game_Core;
 with System_Init; use System_Init;
 with UI.Console; use UI.Console;
+with Ada.Exceptions;
 
 procedure Main is
    -- Глобальные переменные
    Game_Running : Boolean := True;
+   Initialized : Boolean := False;
+   Log_File : File_Type;
    
 begin
-   -- Инициализация системы
-   Initialize_System;
    
-   -- Инициализация игры
-   Initialize_Game;
-   
-   -- Вывод приветственного сообщения
-   Clear_Screen;
-   Put_Line("Добро пожаловать в AdaDND!");
-   Put_Line("--------------------------");
-   Put_Line("Нажмите Enter для начала...");
-   Skip_Line;
-   
-   -- Основной игровой цикл
-   while Game_Running loop
-      -- Обработка событий
-      case Get_User_Input loop
-         when 'q' | 'Q' =>
-            Game_Running := False;
-            Put_Line("Выход из игры...");
-            
-         when 'h' | 'H' =>
-            Show_Help;
-            
-         when 'n' | 'N' =>
-            New_Game;
-            
-         when others =>
-            Process_Command(Get_User_Input);
-      end case;
+   Create(Log_File, Out_File, "game.log");
+   begin
+     -- Инициализация системы
+      Initialize_System;
+      Initialize_Game;
+      Initialized := True;
       
-      -- Обновление состояния игры
-      Update_Game_Loop;
+      -- Вывод приветственного сообщения
+      Clear_Screen;
+      Put_Line("Добро пожаловать в AdaDND!");
+      Put_Line("--------------------------");
+      Put_Line("Нажмите Enter для начала...");
+      Skip_Line;
       
-      -- Рендеринг
-      Render_Game_State;
-   end loop;
+      -- Основной игровой цикл
+      while Game_Running loop
+         declare
+            Input : Character := Get_User_Input;
+         begin
+            case Input is
+               when 'q' | 'Q' =>
+                  Game_Running := False;
+                  Put_Line("Выход из игры...");
+                  
+               when 'h' | 'H' =>
+                  Show_Help;
+                  
+               when 'n' | 'N' =>
+                  New_Game;
+                  
+               when others =>
+                  Process_Command(Input);
+            end case;
+            
+            -- Обновление состояния игры
+            Update_Game_Loop;
+            
+            -- Рендеринг
+            Render_Game_State;
+         end;
+      end loop;
+      
+   exception
+      when E : others =>
+         declare
+            Error_Msg : String := Exception_Information(E);
+         begin
+            Put_Line("Ошибка: " & Error_Msg);
+            Put_Line(Log_File, "Ошибка: " & Error_Msg);
+         end;
+   end;
    
-   -- Завершение игры
-   Shutdown_Game;
-   Shutdown_System;
-   Put_Line("Спасибо за игру!");
-   
-exception
-   when others =>
-      Put_Line("Произошла ошибка. Завершение игры...");
+finally
+   Close(Log_File);
+   if Initialized then
       Shutdown_Game;
       Shutdown_System;
+   end if;
+   Put_Line("Спасибо за игру!");
 end Main;
